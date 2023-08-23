aedcc049e0be4739684dbae501e404378445917b
Mapalo04 <lukundomapalo@gmail.com>
2023-08-23 00:29:39 +0000

    added multiple files

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 
 #ifndef main_h
-#define main_
+#define main_h
 
 #include <stdarg.h>
 #include <stdio.h>
@@ -24,19 +24,20 @@
 */
 struct fmt
 {
-char fmt;
-int (*fn)(va_list, char[], int, int, int, int);
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
 };
 /**
 * typedef struct fmt fmt_t - Struct op
 *
 * @fmt: The format.
 * @fm_t: The function associated.
+*/
 typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
-/****************** FUNCTIONS ******************/
+/*********************************** FUNCTIONS ******************************************/
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
 int flags, int width, int precision, int size);
